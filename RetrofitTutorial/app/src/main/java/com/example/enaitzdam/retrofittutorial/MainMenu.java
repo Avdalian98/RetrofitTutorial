a201e2419b74282250d772a15109157e30bac115
DanielAvdalian <d.avdalian@gmail.com>
2019-01-21 16:18:23 +0100

    MainBase

--- RetrofitTutorial/app/src/main/java/com/example/enaitzdam/retrofittutorial/MainMenu.java
+++ RetrofitTutorial/app/src/main/java/com/example/enaitzdam/retrofittutorial/MainMenu.java
@@ -2,6 +2,7 @@ package com.example.enaitzdam.retrofittutorial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public void ClickMarques(View view) {
+    }
+
+    public void ClickTallers(View view) {
+    }
+
+    public void ClickAveries(View view) {
+    }
+
+    public void ClickListaAveries(View view) {
     }
 }
